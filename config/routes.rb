e0b7932d165e3b61a1284f4b4d193b5c057dd607
Ambreen shafiq <ambren.shafiq@gmail.com>
2024-05-30 11:12:39 +0400

    articles update and edit features

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get 'about', to: 'pages#about'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   # Defines the root path route ("/")
   # root "posts#index"
 end
